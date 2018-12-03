36592118458e5beaade530ec1f4c3aa0d59acf74
SMGuellord <muhiyasumba@gmail.com>
2018-12-03 14:08:08 +0200

    Implemented remove method.

--- binarysearchtree/BSTImplementation.py
+++ binarysearchtree/BSTImplementation.py
@@ -34,16 +34,16 @@ class BinarySearchTree(object):
 
     def get_min(self, node):
         if node.left_child:
-            return self.get_min()
+            return self.get_min(node.left_child)
         return node.data
 
     def get_max_value(self):
         if self.root:
-            return self.get_man(self.root)
+            return self.get_max(self.root)
 
     def get_max(self, node):
         if node.right_child:
-            return self.get_max()
+            return self.get_max(node.right_child)
         return node.data
 
     def traverse(self):
@@ -54,6 +54,59 @@ class BinarySearchTree(object):
         if node.left_child:
             self.traverse_in_order(node.left_child)
         print("%s " % node.data)
-        
+
         if node.right_child:
             self.traverse_in_order(node.right_child)
+
+    def remove(self, data):
+        if self.root:
+            self.root = self.remove_node(data, self.root)
+
+    def remove_node(self, data, node):
+        if not node:
+            return node
+        if data < node.data:
+            node.left_child = self.remove_node(data, node.left_child)
+        elif data > node.data:
+            node.right_child = self.remove_node(data, node.right_child)
+        else:
+            if not node.left_child and not node.right_child:
+                print("Removing a leaf node...")
+                del node
+                return None
+            if not node.left_child:
+                print("Removing a node with single right child...")
+                temp_node = node.right_child
+                del node
+                return temp_node
+            elif not node.right_child:
+                print("Removing a node with single left child...")
+                temp_node = node.left_child;
+                del node
+                return temp_node
+            print("Removing node with two children...")
+            temp_node = self.get_predecessor(node.left_child)
+            node.data = temp_node.data
+            node.left_child = self.remove_node(temp_node.data, node.left_child)
+
+        return node
+
+    def get_predecessor(self, node):
+        if node.right_child:
+            return self.get_predecessor(node.right_child)
+        return node
+
+
+bst = BinarySearchTree()
+
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(6)
+bst.insert(1)
+
+print("Min value:", bst.get_min_value())
+print("Max value:", bst.get_max_value())
+bst.remove(10)
+
+bst.traverse()
